f47ef5846ffab038fca668d33e1e09a59ed5fc1d
keremcalki@gmail.com <keremcalki@gmail.com>
2023-11-10 15:12:40 +0900

    fix: px to rem function for spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const BASE_PIXEL = 16;
+const pxToRem = (px, base = BASE_PIXEL) => `${px / base}rem`;
+const rem = [...Array(5000).keys()].reduce((acc, px) => {
+  acc[`${px}pxr`] = pxToRem(px);
+  return acc;
+}, {});
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: ["class", '[data-mode="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        ...rem,
+      },
+    },
   },
   plugins: [],
   corePlugins: {
