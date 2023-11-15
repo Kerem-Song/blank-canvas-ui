98a04caff3c6e94b02977491b9b82b0c4cb774d8
john-JongikYoon <jiyoon@lunasoft.co.kr>
2023-11-15 15:14:49 +0900

    chore: eslint, prettier, vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,36 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
-import dts from "vite-plugin-dts";
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@models": path.resolve(__dirname, "./src/models"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
     },
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "component-package",
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'component-package',
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
@@ -34,4 +38,9 @@ export default defineConfig({
     emptyOutDir: true,
   },
   plugins: [react(), dts()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
 });
