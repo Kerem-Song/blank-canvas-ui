ad11f88929282f042d7fd47be0865244eb7ca708
ellie <kjh90128@gmail.com>
2023-11-15 17:03:53 +0900

    feat: index 파일 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export { Button } from "@components";
-export { Radio } from "@components";
-export { Textarea } from "@components";
-export { TextAreaWithTitleCounter } from "@components";
-export * from "./index.css";
+export { Button } from '@components';
+export { Radio } from '@components';
+export { Textarea } from '@components';
+export { TextAreaWithTitleCounter } from '@components';
+export { Flex } from '@components';
+export * from './index.css';
