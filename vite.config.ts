6d6bb04d5be1ba7c354000dbd75e93f6983c58d2
keremcalki@gmail.com <keremcalki@gmail.com>
2023-11-15 16:26:39 +0900

    fix: ts config paths

--- vite.config.ts
+++ vite.config.ts
@@ -9,17 +9,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    // alias: {
-    //   '@': path.resolve(__dirname, './src'),
-    //   '@components': path.resolve(__dirname, './src/components'),
-    //   '@models': path.resolve(__dirname, './src/models'),
-    //   '@modules': path.resolve(__dirname, './src/modules'),
-    //   '@styles': path.resolve(__dirname, './src/styles'),
-    //   '@assets': path.resolve(__dirname, './src/assets'),
-    //   '@hooks': path.resolve(__dirname, './src/hooks'),
-    // },
-  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
