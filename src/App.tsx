4e5dadd087c1bdbb28be8f0b95f292c94726b1b0
keremcalki@gmail.com <keremcalki@gmail.com>
2023-11-29 15:14:18 +0900

    fix: reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 
-import { Tooltip } from '@components';
 import { IRadioOption } from '@components/data-entry/Radio/RadioGroup';
 import { useState } from 'react';
 
@@ -13,14 +12,6 @@ function App() {
   ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
@@ -29,35 +20,6 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <Textarea className="" maxLength={12} autoComplete={'true'} />
-      <TextAreaWithTitleCounter
-        label="label"
-        direction={'inside'}
-        showCount={true}
-        maxLength={20}
-      />
-      <Button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        button..
-      </Button>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        ..
-      </button>
-      <TextAreaWithTitleCounter
-        label="top"
-        direction="top"
-        showCount={true}
-        maxLength={20}
-      />
-      <TextAreaWithTitleCounter
-        label="bottom"
-        direction="bottom"
-        showCount={true}
-        maxLength={20}
-      />
-      <Radio>asdf</Radio>
-      <RadioGroup options={option} name="haha" vertical={false} gap={20} />
-      <Input isSearch={true} disabled={true} />
-      <Switch />
     </>
   );
 }
