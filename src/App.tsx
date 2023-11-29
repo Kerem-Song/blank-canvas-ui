d8df9e06c4c1b506d4a70e8765ab37c4a071c217
keremcalki@gmail.com <keremcalki@gmail.com>
2023-11-29 15:56:50 +0900

    fix: yarn rc for storybook-addon-react-router-v6

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,7 @@ import {
 import { IRadioOption, RadioGroup } from '@components/data-entry/Radio/RadioGroup';
 import { useState } from 'react';
 
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
   const option: IRadioOption[] = [
     { label: '1', value: '1' },
     { label: '2', value: 2 },
@@ -25,22 +20,6 @@ function App() {
   ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <Textarea className="" maxLength={12} autoComplete={'true'} />
       <TextAreaWithTitleCounter
         label="label"
