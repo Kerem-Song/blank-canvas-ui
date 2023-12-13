5b389d979ea78f42ac2d35f6d6a60c0a0a67493a
keremcalki@gmail.com <keremcalki@gmail.com>
2023-12-13 18:48:45 +0900

    fix: tsconfig include, eslintrc.cjs project in parseOption

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    // project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
   extends: [
