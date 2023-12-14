dd096e65be9251d62867434a07774bd03bf25122
ellie <jhkim@lunasoft.co.kr>
2023-12-14 14:52:38 +0900

    fix: index파일에 추가

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,6 @@ export { Card } from '@components';
 export { Badge } from '@components';
 export { Row } from '@components';
 export { Col } from '@components';
+export { Select } from '@components';
+export { Tooltip } from '@components';
 export * from '@components/general/button';
