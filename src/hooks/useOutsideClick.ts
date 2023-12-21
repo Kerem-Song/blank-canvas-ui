fbfb5192c686f99ea01427521246db1a267f114f
keremcalki@gmail.com <keremcalki@gmail.com>
2023-12-21 11:37:56 +0900

    fix: useOutsideClick

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -5,18 +5,7 @@ export function useOutsideClick(ref: any, action: () => void, condition?: boolea
     document.addEventListener('click', (e) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
       if (ref.current && !ref.current.contains(e.target)) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        console.log('@e target', e.target);
-
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        console.log('@contain', ref.current?.contains(e.target));
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        console.log('@ref', ref, ref?.current);
         action();
-      }
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-      if (ref.current && ref.current.contains(e.target)) {
-        console.log('@포함');
       }
     });
     return () => {
