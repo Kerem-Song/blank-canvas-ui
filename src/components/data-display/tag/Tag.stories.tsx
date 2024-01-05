89a3b62869384d1e5aff0a11eb03a2ce778af904
ellie <jhkim@lunasoft.co.kr>
2024-01-05 16:55:55 +0900

    fix: css, type 분리, 파일, 폴더명 변경

---
+++ src/components/data-display/tag/Tag.stories.tsx
@@ -0,0 +1,39 @@
+import { Flex, ITagProps, Tag } from '@components';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<ITagProps> = {
+  title: 'components/data-display/Tag',
+  component: Tag,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    componentSubtitle: '태그 표시',
+  },
+};
+
+export default meta;
+type Story = StoryObj<ITagProps>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <Flex
+        style={{ height: '100px', border: '1px solid' }}
+        justify="space-evenly"
+        align="center"
+      >
+        <Tag>Tag1</Tag>
+        <Tag bordered={false}>none-bordered</Tag>
+        <Tag
+          closeIcon={true}
+          onClose={() => {
+            alert('클릭');
+          }}
+        >
+          close TAG
+        </Tag>
+        <Tag color="red">Color tag</Tag>
+      </Flex>
+    );
+  },
+};
