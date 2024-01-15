105c83ce73e4f5f03eccd142c8a403ebeed1ef6e
ellie <jhkim@lunasoft.co.kr>
2024-01-15 12:32:19 +0900

    fix: tag css 수정

--- src/components/data-display/tag/Tag.tsx
+++ src/components/data-display/tag/Tag.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { forwardRef, useEffect, useRef, useState } from 'react';
 
 import { ITagProps } from './Tag.types';
+import { tagClasses } from './tagClasses';
 
 export const Tag = forwardRef<HTMLSpanElement, ITagProps>((args, ref) => {
   const {
@@ -36,17 +37,29 @@ export const Tag = forwardRef<HTMLSpanElement, ITagProps>((args, ref) => {
     };
   }, []);
 
+  const rootClassName = classNames(tagClasses.root, {
+    [tagClasses.option.bordered]: bordered,
+  });
+
+  const iconClassName = classNames(
+    tagClasses.option.pd,
+    tagClasses.option.icon,
+    icon ? [tagClasses.option.inline] : [tagClasses.option.hidden],
+  );
+
+  const closeClassName = classNames(
+    tagClasses.option.close,
+    tagClasses.option.pd,
+    closeIcon ? [tagClasses.option.inline] : [tagClasses.option.hidden],
+  );
+
   return (
     <>
       {showOptions ? (
         <span
           ref={ref}
           {...props}
-          className={classNames(
-            'bc-tag-area',
-            { 'bc-tag-bordered': bordered },
-            className,
-          )}
+          className={classNames(rootClassName, className)}
           style={{
             ...style,
             backgroundColor: color,
@@ -54,21 +67,16 @@ export const Tag = forwardRef<HTMLSpanElement, ITagProps>((args, ref) => {
             color: color ? 'white' : 'auto',
           }}
         >
-          <span
-            {...props}
-            className={classNames('bc-tag-pd', 'bc-tag-icon')}
-            style={{ display: icon ? 'bc-tag-inline' : 'none' }}
-          >
+          <span {...props} className={classNames(iconClassName)}>
             {icon}
           </span>
           {children}
           <span
             {...props}
-            className={classNames('bc-tag-close', 'bc-tag-pd')}
+            className={classNames(closeClassName)}
             onClick={onClose}
             ref={closeRef}
             style={{
-              display: closeIcon ? 'bc-tag-inline' : 'none',
               color: color ? 'white' : 'auto',
             }}
           >
